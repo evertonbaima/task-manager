11215e8769aeb8873768fe5f15b90f6d55b3bdeb
Everton Baima <everton-baima@hotmail.com>
2014-04-10 09:56:08 -0300

    Preparing Helper Classes

--- includes/Config.php
+++ includes/Config.php
@@ -1,2 +1,13 @@
 <?php
 
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'task_manager');
+
+define('USER_CREATED_SUCCESSFULLY', 0);
+define('USER_CREATE_FAILED', 1);
+define('USER_ALREADY_EXISTED', 2);
